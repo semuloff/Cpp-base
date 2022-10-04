3fed12df8813bb2e88e01339232d0a2cef41e3fc
SEMUL <semul.offical@icloud.com>
2022-10-04 20:28:22 +0300

    Understanding variables

---
+++ lesson_2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+
+int main() {
+    int number;
+
+    number = 34;
+
+    char symbol = '2';
+
+    // float f_numb = (float)3.324;
+
+    float f_numb = 3.324f;
+
+    double d_numb = 3.1313;
+
+    bool flag = true;
+    flag = false;
+
+    const float const_f_numb = 2.3424f;
+
+    /* This is comment */
+
+}
+
