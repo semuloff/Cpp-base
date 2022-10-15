ad04ec28f02f15363f01bc1df032b6e50426a5ad
SEMUL <semul.offical@icloud.com>
2022-10-15 15:57:42 +0300

    one-dimensional arrays

---
+++ lesson_12.cpp
@@ -0,0 +1,20 @@
+# include <iostream>
+
+using namespace std;
+
+int main() {
+    // one-dimensional arrays
+    const int lenght = 10;
+    int arr[lenght];
+
+    for (int index = 0; index < lenght; index++) {
+        cout << "Enter the index's value" << endl << "index: " << index << ": ";
+        cin >> arr[index];
+    }
+
+    cout << "-------------------------------------------" << endl;
+
+    for (int index = 0; index < lenght; index++) {
+        cout << "index: " << index << ", value: " << arr[index] << endl;
+    }
+}
