b482d4c33b721faa8fad93a5d837f42d16f461a8
SEMUL <semul.offical@icloud.com>
2022-10-23 16:56:27 +0300

    structure concept

---
+++ lesson_15.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    struct Human
+    {
+        string firstName;
+        string lastName;
+        string post;
+        int age;
+        float growth;
+    };
+
+    // nested structures.
+    struct Team
+    {
+        Human teamLeader;
+        Human frontendDeveloper;
+        Human backendDeveloper;
+    };
+
+    // create an instance of the structure.
+    Team hyperColor;
+
+    // fill in the data for a specific team member
+    hyperColor.teamLeader.age = 26;
+    hyperColor.teamLeader.growth = 176.45F;
+    hyperColor.teamLeader.firstName = "Mark";
+    hyperColor.teamLeader.lastName = "Menson";
+
+    cout << hyperColor.teamLeader.firstName << "'s age: ";
+    cout << hyperColor.teamLeader.age << endl;
+    
+
+
+    
+
+}
